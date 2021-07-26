da6c04224a976c4416aee206f2e823fc7fc0f46e
DONG WOONG OH <integral5137@gmail.com>
2021-07-26 23:54:35 +0900

    Create to Sign up form

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const SignUp = () => {
-  return <section>SignUp</section>;
+  return (
+    <section>
+      <form>
+        <li>
+          <input type="text" name="id" placeholder="ID" autoComplete="off" />
+        </li>
+        <li>
+          <input
+            type="password"
+            name="pass1"
+            placeholder="Password"
+            autoComplete="off"
+          />
+        </li>
+        <li>
+          <input
+            type="password"
+            name="pass2"
+            placeholder="Confirm password"
+            autoComplete="off"
+          />
+        </li>
+        <li>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            autoComplete="off"
+          />
+        </li>
+        <li>
+          <input
+            type="text"
+            name="nick"
+            placeholder="Nickname"
+            autoComplete="off"
+          />
+        </li>
+        <li>
+          <input type="submit" value="Sign up" />
+        </li>
+      </form>
+    </section>
+  );
 };
 
 export default SignUp;

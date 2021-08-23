435d06bca4654dbd4d4fa17bd4be74b0221a46c9
DONG WOONG OH <integral5137@gmail.com>
2021-08-23 12:38:49 +0900

    Update video context in the reducer

--- client/src/contexts/VideoContexts.tsx
+++ client/src/contexts/VideoContexts.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { createContext } from "react";
+import { useReducer } from "react";
+import {
+  Action,
+  State,
+  VideoFormDispatch,
+} from "../types/Video/VideoForm.type";
 
-const VideoContexts = () => {
-  return <section></section>;
+export const VideoStateContext = createContext<State | null>(null);
+export const VideoDispatchContext = createContext<VideoFormDispatch | null>(
+  null
+);
+
+function videoReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "VIDEO_UPLOAD":
+      return {
+        ...state.uploadForm,
+        uploadForm: action.uploadForm,
+      };
+
+    default:
+      throw new Error("Unhandle actions");
+  }
+}
+
+const initVideoForm: State = {
+  uploadForm: {
+    video: null,
+    title: "",
+    description: "",
+    theme: "",
+  },
+};
+
+const VideoContexts: React.FC = ({ children }) => {
+  //
+  const [state, dispatch] = useReducer(videoReducer, initVideoForm);
+
+  return (
+    <VideoStateContext.Provider value={state}>
+      <VideoDispatchContext.Provider value={dispatch}>
+        {children}
+      </VideoDispatchContext.Provider>
+    </VideoStateContext.Provider>
+  );
 };
 
 export default VideoContexts;
--- client/src/types/Video/VideoForm.type.ts
+++ client/src/types/Video/VideoForm.type.ts
@@ -1,5 +1,5 @@
 import { Dispatch } from "react";
-export type VideoForm = {
+export type VideoFormState = {
   video: File | null;
   title: string;
   description: string;
@@ -7,22 +7,9 @@ export type VideoForm = {
 };
 
 export type State = {
-  videoData?: Pick<VideoForm, "video">;
-  titleData?: Pick<VideoForm, "title">;
-  descriptionData?: Pick<VideoForm, "description">;
-  theme?: Pick<VideoForm, "theme">;
+  uploadForm: VideoFormState;
 };
 
-export type Action =
-  | {
-      type: "VIDEO_UPLOAD";
-      videoData?: Pick<VideoForm, "video">;
-    }
-  | { type: "TITLE_UPLOAD"; titleData?: Pick<VideoForm, "title"> }
-  | {
-      type: "DESCRIPTION_UPLOAD";
-      descriptionData?: Pick<VideoForm, "description">;
-    }
-  | { type: "THEME_UPLOAD"; theme?: Pick<VideoForm, "theme"> };
+export type Action = { type: "VIDEO_UPLOAD"; uploadForm: VideoFormState };
 
 export type VideoFormDispatch = Dispatch<Action>;
